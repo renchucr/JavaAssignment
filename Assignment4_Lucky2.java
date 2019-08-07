1d3e27ea5655f188ca53b452b0065ad981e77326
renchucr <53008968+renchucr@users.noreply.github.com>
2019-08-07 15:41:32 +0530

    Updated
    Updated

--- Assignment4_Lucky2.java
+++ Assignment4_Lucky2.java
@@ -8,7 +8,7 @@ public class Assignment4_Lucky2 {
 
 	public static void main(String[] args) {
 
-		int  reminder, quotient = 0, length = 0;
+		int length = 0;
 		Boolean flag = true;
 		int[] arnumber;
 
@@ -21,58 +21,15 @@ public class Assignment4_Lucky2 {
 		arnumber = new int[length];
 
 		for (int index = 0; index < length; index++) {
-			System.out.println("Please enter " + index + " position value");
+			System.out.println("Please enter " + (index+1) + " position value");
 			arnumber[index] = scan.nextInt(); // value
 		}
 
-		
-
-		int[] frequency = new int[10]; // array
-		for (int index = 0; index < length; index++) {
-			quotient = arnumber[index];
-
-			do {
-
-				reminder = quotient % 10;
-				quotient = quotient / 10;
-				switch (reminder) {
-				case 0:
-					frequency[0] += 1;
-					break;
-				case 1:
-					frequency[1] += 1;
-					break;
-				case 2:
-					frequency[2] += 1;
-					break;
-				case 3:
-					frequency[3] += 1;
-					break;
-				case 4:
-					frequency[4] += 1;
-					break;
-				case 5:
-					frequency[5] += 1;
-					break;
-				case 6:
-					frequency[6] += 1;
-					break;
-				case 7:
-					frequency[7] += 1;
-					break;
-				case 8:
-					frequency[8] += 1;
-					break;
-				case 9:
-					frequency[9] += 1;
-					break;
-
-				}
-			} while (quotient > 0);
-
-			for (int i = 0; i < 9; i++) {
-				if (frequency[i] > 1) {
+		for (int i = 0; i < length; i++) {
+			for (int j = i + 1; j < length; j++) {
+				if (arnumber[j] == arnumber[i]) {
 					flag = false;
+					break;
 				}
 			}
 		}
